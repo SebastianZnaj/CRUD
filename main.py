838e031ef1f45bfeb3f715f279a46a55c7e93e2b
Sebastian Znaj <seb.znaj@gmail.com>
2017-04-03 15:17:30 +0200

    Fill up files with basic syntax

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect, url_for
+from models.list import ItemList
+app = Flask(__name__)
+
+
+@app.route("/")
+def list():
+    """ Shows list of todo items stored in the database.
+    """
+    items = ItemList.get_all()
+    return render_template("index.html", items=items)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
---
+++ models/model.py
@@ -0,0 +1,23 @@
+class ItemList:
+
+    def __init__(self, name, description, price):
+        self.id = None
+        self.name = name
+        self.description = description
+        self.price = price
+
+    def get_all(self):
+        pass
+
+    def get_by_id(self):
+        pass
+
+    def add(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def edit(self):
+        pass
+
